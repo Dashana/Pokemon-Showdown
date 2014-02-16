cdc6cbb6967c28807d168c2d0af9442b755c3473
jd4564 <thelittlejd09@gmail.com>
2014-02-16 18:05:20 -0400

    Add in /giveavatar

--- users.js
+++ users.js
@@ -723,9 +723,29 @@ var User = (function () {
 			if (body !== '1') {
 				authenticated = true;
 
-				if (config.customavatars && config.customavatars[userid]) {
+				self = this;
+				function setAvatar(data) {
+					var line = data.split('\n');
+					for (var u in line) {
+						var row = line[u].split(',');
+						if (row[0] == userid) {
+							self.avatar = row[1];
+							break;
+						}
+					}
+				}				
+
+				avatar = fs.readFile('config/avatars.csv', 'utf8', function read(err, data) {
+					if (err) {
+						console.log('Error reading avatars.csv: '+err);
+					} else {
+						setAvatar(data);
+					}
+				});
+
+				/*if (config.customavatars && config.customavatars[userid]) {
 					avatar = config.customavatars[userid];
-				}
+				}*/
 
 				if (usergroups[userid]) {
 					group = usergroups[userid].substr(0,1);
